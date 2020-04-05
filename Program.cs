fb2bdf52c0dc9cebcd263ad7a991ed0921a4fb6e
thantrieu <trieuthan92pro@gmail.com>
2020-04-05 11:11:06 +0700

    Add draw heart in console app

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "My First App In C#";
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("*****************************");
+            Console.WriteLine("***     0 0 0   0 0 0     ***");
+            Console.WriteLine("***   0 0 0 0 0 0 0 0 0   ***");
+            Console.WriteLine("*** 0 0 0 0 0 0 0 0 0 0 0 ***");
+            Console.WriteLine("*** 0 0 0 0 0 0 0 0 0 0 0 ***");
+            Console.WriteLine("***   0 0 0 0 0 0 0 0 0   ***");
+            Console.WriteLine("***     0 0 0 0 0 0 0     ***");
+            Console.WriteLine("***       0 0 0 0 0       ***");
+            Console.WriteLine("***         0 0 0         ***");
+            Console.WriteLine("***           0           ***");
+            Console.WriteLine("*****************************");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Gray;
+        }
+    }
+}
