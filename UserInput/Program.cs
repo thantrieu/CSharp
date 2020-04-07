608aac5ac37ea64b2f212cc7f2326792e0a30be0
thantrieu <trieuthan92pro@gmail.com>
2020-04-07 23:12:07 +0700

    Add Data types

---
+++ UserInput/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // full name, age, avg mark
+            Console.WriteLine("Nhap ho va ten: ");
+            string fullName = Console.ReadLine();
+
+            Console.WriteLine("Nhap tuoi: ");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            int age = System.Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nhap diem TB: ");
+            //float avgMark = Convert.ToSingle(Console.ReadLine());
+            float avgMark = System.Single.Parse(Console.ReadLine());
+
+            Console.WriteLine("======================================");
+            Console.WriteLine("Hello " + fullName);
+            Console.WriteLine("I am " + age);
+            Console.WriteLine("My avg mark is " + avgMark);
+        }
+    }
+}
