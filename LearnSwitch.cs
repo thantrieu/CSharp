f0dd7edb013dd852a3a85d012bc6629a298c59c9
thantrieu <trieuthan92pro@gmail.com>
2020-04-12 11:59:27 +0700

    Add switch control

---
+++ LearnSwitch.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controls
+{
+    class LearnSwitch
+    {
+        static void Main()
+        {
+            int dayOfWeek = Convert.ToInt32(Console.ReadLine());
+            switch (dayOfWeek)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+
+                default:
+                    Console.WriteLine("Nhap vao dung ngay trong tuan(1-7)");
+                    break;
+            }
+        }
+
+    }
+}
