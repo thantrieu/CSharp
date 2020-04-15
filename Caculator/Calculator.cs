69f2a12f3c11ca50da4210b8cd0c7b484642e5d2
thantrieu <trieuthan92pro@gmail.com>
2020-04-15 22:28:02 +0700

    Add new calculator

--- Caculator/Calculator.cs
+++ Caculator/Calculator.cs
@@ -4,16 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Caculator
+namespace Calculator
 {
-    class Caculator
+    class Calculator
     {
         static void Main(string[] args)
         {
             int choice = 0;
             do
             {
-                Console.WriteLine("============= MY CACULATOR =============");
+                Console.WriteLine("===================== MAY TINH CA NHAN =====================");
                 Console.WriteLine("1. Cong hai so.");
                 Console.WriteLine("2. Tru hai so.");
                 Console.WriteLine("3. Nhan hai so.");
@@ -25,75 +25,77 @@ namespace Caculator
 
                 string input = Console.ReadLine();
                 choice = Convert.ToInt32(input);
-                int a = default, b = 0;
+                int a = 0, b = 0;
 
                 switch (choice)
                 {
                     case 0:
-                        Console.WriteLine("======= HEN GAP LAI =======");
+                        Console.WriteLine("============= HEN GAP LAI =============");
                         break;
 
                     case 1:
-                        Console.WriteLine("Nhap so a: ");
+                        Console.WriteLine("Nhap so nguyen a: ");
                         a = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Nhap so b: ");
+                        Console.WriteLine("Nhap so nguyen b: ");
                         b = Convert.ToInt32(Console.ReadLine());
                         Console.WriteLine($"{a} + {b} = {a + b}");
                         break;
 
                     case 2:
-                        Console.WriteLine("Nhap so a: ");
+                        Console.WriteLine("Nhap so nguyen a: ");
                         a = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Nhap so b: ");
+                        Console.WriteLine("Nhap so nguyen b: ");
                         b = Convert.ToInt32(Console.ReadLine());
                         Console.WriteLine($"{a} - {b} = {a - b}");
                         break;
+
                     case 3:
-                        Console.WriteLine("Nhap so a: ");
+                        Console.WriteLine("Nhap so nguyen a: ");
                         a = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Nhap so b: ");
+                        Console.WriteLine("Nhap so nguyen b: ");
                         b = Convert.ToInt32(Console.ReadLine());
                         Console.WriteLine($"{a} * {b} = {a * b}");
                         break;
 
                     case 4:
-                        Console.WriteLine("Nhap so a: ");
+                        Console.WriteLine("Nhap so nguyen a: ");
                         a = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Nhap so b: ");
+                        Console.WriteLine("Nhap so nguyen b: ");
                         b = Convert.ToInt32(Console.ReadLine());
                         if(b == 0)
                         {
-                            Console.WriteLine("Nhap mau so khac 0!");
+                            Console.WriteLine("Mau so b phai khac 0!");
                             continue;
                         }
-                        Console.WriteLine($"{a} / {b} = {1.0f*a / b}");
+                        Console.WriteLine($"{a} / {b} = {1.0f * a / b}");
                         break;
 
                     case 5:
-                        Console.WriteLine("Nhap so a: ");
+                        Console.WriteLine("Nhap so nguyen a: ");
                         a = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Nhap so b: ");
+                        Console.WriteLine("Nhap so nguyen b: ");
                         b = Convert.ToInt32(Console.ReadLine());
                         if (b == 0)
                         {
-                            Console.WriteLine("Nhap mau so khac 0!");
+                            Console.WriteLine("Mau so b phai khac 0!");
                             continue;
                         }
                         Console.WriteLine($"{a} % {b} = {a % b}");
                         break;
 
                     case 6:
-                        Console.WriteLine("Nhap so a: ");
+                        Console.WriteLine("Nhap so nguyen a: ");
                         a = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Nhap so b: ");
+                        Console.WriteLine("Nhap so nguyen b: ");
                         b = Convert.ToInt32(Console.ReadLine());
                         Console.WriteLine($"{a} ^ {b} = {Math.Pow(a, b)}");
                         break;
 
                     default:
-                        Console.WriteLine("Nhap dung chuc nang (0 - 6).");
+                        Console.WriteLine("Nhap cac chuc nang tu 1 den 6!");
                         break;
                 }
+
             } while (choice != 0);
         }
     }
