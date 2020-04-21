1e6d296c6216c6873520ed357096e22cc9a29b1d
Trieu Than <trieuthan92pro@gmail.com>
2020-04-21 08:55:16 +0700

    Update method overloading

--- Method/Overloading.cs
+++ Method/Overloading.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +12,39 @@ namespace Methods
         static void Main()
         {
             // Nạp chồng phương thức
+            int sum = Add(120, 300);
+            float s2 = Add(12, 12.36f);
+            long s3 = Add(10L, 20, 6);
+        }
 
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        static float Add(int b, float a)
+        {
+            return a + b;
+        }
+
+        static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        static long Add(long a, long b)
+        {
+            return a + b;
+        }
+
+        static long Add(long a, int b, short c)
+        {
+            return a + b + c;
+        }
+
+        static long Add(long a, long b, short c)
+        {
+            return a + b + c;
         }
     }
 }
