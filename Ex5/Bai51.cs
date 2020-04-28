afa697bce4ee203e3c5542c13a8032ea264ee3b7
Trieu Than <trieuthan92pro@gmail.com>
2020-04-28 23:04:26 +0700

    Add ex5.1

---
+++ Ex5/Bai51.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodExercises
+{
+    class Bai51
+    {
+        static void Main()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            bool kq = ktraNgTo(n);
+            if(kq)
+            {
+                Console.WriteLine($"{n} la so nguyen to");
+            } else
+            {
+                Console.WriteLine($"{n} khong phai so nguyen to");
+            }
+        }
+
+        static bool ktraNgTo(int n)
+        {
+            if(n < 2) { 
+                return false; 
+            }
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if(n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
