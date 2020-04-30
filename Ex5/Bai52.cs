e044c45f401a31734ca84062d8596c0bce4fb5a8
Trieu Than <trieuthan92pro@gmail.com>
2020-04-30 21:09:34 +0700

    add exercises 5.2, 5.3

---
+++ Ex5/Bai52.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodExercises
+{
+    // 1234321
+    class Bai52
+    {
+        static void Main()
+        {
+            int n = nhapN();
+            hienThiKQ(n);
+            Console.WriteLine();
+        }
+
+        static void hienThiKQ(int n)
+        {
+            int count = 0;
+            int i = 2;
+            while(count < n)
+            {
+                if(ktraNgto(i))
+                {
+                    count++;
+                    Console.Write(i + " ");
+                }
+                i++;
+            }
+        }
+
+        static bool ktraNgto(int n)
+        {
+            if(n < 2)
+            {
+                return false;
+            }
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if(n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static int nhapN()
+        {
+            int n = 0;
+            while(n <= 0)
+            {
+                Console.WriteLine("Nhap n > 0: ");
+                n = Convert.ToInt32(Console.ReadLine());
+                if(n > 0)
+                {
+                    break;
+                }
+            }
+            return n;
+        }
+    }
+}
---
+++ Ex5/Bai53.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodExercises
+{
+    class Bai53
+    {
+        static void Main()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            n = Math.Abs(n);
+            bool result = thuanNghich(n);
+            if(result)
+            {
+                Console.WriteLine($"{n} la so thuan nghich");
+            } else
+            {
+                Console.WriteLine($"{n} khong la so thuan nghich");
+            }
+        }
+
+        // 1234
+        // dao: 4321
+        // m: 0
+        static bool thuanNghich(int n)
+        {
+            int dao = 0;
+            int m = n;
+            while(m > 0)
+            {
+                dao = dao * 10 + m % 10;
+                m /= 10;
+            }
+            return dao == n;
+        }
+    }
+}
