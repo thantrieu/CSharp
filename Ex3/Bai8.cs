75bd47682032f3786bca7f1e399db4e82bfcc678
Trieu Than <trieuthan92pro@gmail.com>
2020-05-04 18:05:51 +0700

    Add ex3.8 - 3.9

---
+++ Ex3/Bai8.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises3
+{
+    class Bai8
+    {
+        static void Main()
+        {
+            Console.WriteLine("Nhap chieu cao h: ");
+            int h = Convert.ToInt32(Console.ReadLine());
+            if(h > 0)
+            {
+                for (int i = 1; i <= h; i++)
+                {
+                    for (int j = 1; j < 2*h; j++)
+                    {
+                        if(Math.Abs(h - j) < i)
+                        {
+                            Console.Write(" * ");
+                        } else
+                        {
+                            Console.Write("   ");
+                        }
+                    }
+                    Console.WriteLine("\n");
+                }
+            } else
+            {
+                Console.WriteLine("Nhap chieu cao h > 0");
+            }
+        }
+    }
+}
---
+++ Ex3/Bai9.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises3
+{
+    class Bai9
+    {
+        static void Main()
+        {
+            Console.WriteLine("Nhap h > 0: ");
+            int h = Convert.ToInt32(Console.ReadLine());
+            if(h > 0)
+            {
+                for (int i = 1; i <= h; i++)
+                {
+                    for (int j = 1; j < 2*h; j++)
+                    {
+                        int index = Math.Abs(h - j);
+                        if(index < i)
+                        {
+                            Console.Write(" " + Math.Abs(i - index) + " ");
+                        } else
+                        {
+                            Console.Write("   ");
+                        }
+                    }
+                    Console.WriteLine("\n");
+                }
+            } else
+            {
+                Console.WriteLine("Nhap chieu cao h > 0");
+            }
+        }
+    }
+}
