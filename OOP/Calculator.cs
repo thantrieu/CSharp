e52d08e70b8db02ecb01904701c75060ac3c2d53
Trieu Than <trieuthan92pro@gmail.com>
2020-05-12 17:02:34 +0700

    Add static class

---
+++ OOP/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    static class Calculator
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Mul(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static float Div(int a, int b)
+        {
+            return (float)a / b;
+        }
+    }
+}
---
+++ OOP/StaticClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using static OOP.Calculator;
+using static System.Console;
+
+namespace OOP
+{
+    class StaticClass
+    {
+        static void Main(string[] args)
+        {
+            int a = 120, b = 500;
+            WriteLine("a + b = " + Add(a, b));
+        }
+    }
+}
